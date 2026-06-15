175f0805b6e95a9efc0b5bc5aed2092c4ac8e917
Craig Jacob <cjbuss07@gmail.com>
2026-06-15 14:35:30 -0500

    assignment

--- src/main/java/com/itccloud/mwccdc/controller/HomeController.java
+++ src/main/java/com/itccloud/mwccdc/controller/HomeController.java
@@ -1,11 +1,20 @@
 package com.itccloud.mwccdc.controller;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
 	@GetMapping("/")
 	public String index() {
+		
+		String time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+		System.out.println(time);
+		
 		return "index-form";
 		
 
